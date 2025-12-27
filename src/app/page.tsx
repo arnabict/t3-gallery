676169c23b70b8d50d0a9284824ddbf914cf2d8b
Arnab Paul <arnabict@outlook.com>
2025-12-27 21:08:06 +0100

    feat: start implementing parallel routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import { getMyImages } from "~/server/queries";
 
 // const mockUrls = [
@@ -22,13 +23,17 @@ async function Images() {
   return (
     <div className="felx-wrap flex justify-center gap-4">
       {images.map((image) => (
-        <div key={image.id} className="relative flex h-48 w-48 flex-col">
-          <Image
-            src={image.url}
-            style={{ objectFit: "contain" }}
-            fill
-            alt={image.name}
-          />
+        <div key={image.id} className="flex h-48 w-48 flex-col">
+          <Link href={`/img/${image.id}`}>
+            <Image
+              src={image.url}
+              style={{ objectFit: "contain" }}
+              width={192}
+              height={192}
+              fill
+              alt={image.name}
+            />
+          </Link>
           <div>{image.name}</div>
         </div>
       ))}
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -13,3 +13,19 @@ export async function getMyImages() {
   });
   return images;
 }
+
+export async function getImage(id: number) {
+  const user = auth();
+
+  if (!(await user).userId) throw new Error("Unauthorized!");
+
+  const image = await db.query.images.findFirst({
+    where: (modal, { eq }) => eq(modal.id, id),
+  });
+
+  if (!image) throw new Error("Image not found!");
+
+  if (image.userId !== (await user).userId) throw new Error("Unauthorized!");
+
+  return image;
+}
